161b376f5301b8d17b88583beb78d9c89e934a63
Your Name <you@example.com>
2021-09-06 16:27:23 +0300

    finished page

---
+++ js/modal.js
@@ -0,0 +1,108 @@
+(function () {
+    if (typeof window.CustomEvent === "function") return false;
+    function CustomEvent(event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: null };
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+    }
+    window.CustomEvent = CustomEvent;
+})();
+
+$modal = function (options) {
+    var
+        _elemModal,
+        _eventShowModal,
+        _eventHideModal,
+        _hiding = false,
+        _destroyed = false,
+        _animationSpeed = 200;
+
+    function _createModal(options) {
+        var
+            elemModal = document.createElement('div'),
+            modalTemplate = '<div class="modal__backdrop" data-dismiss="modal"><form id="modal_form" method="post" enctype="multipart/form-data"><div class="modal__content"><div class="modal__header"><div class="modal__title" data-modal="title">{{title}}</div><span class="modal__btn-close" data-dismiss="modal" title="Закрыть">×</span></div><div class="modal__body" data-modal="content">{{content}}</div>{{footer}}</div></form></div>',
+            modalFooterTemplate = '<div class="modal__footer">{{buttons}}</div>',
+            modalButtonTemplate = '<button type="button" class="{{button_class}}" data-handler={{button_handler}}>{{button_text}}</button>',
+            modalHTML,
+            modalFooterHTML = '';
+
+        elemModal.classList.add('modal');
+        modalHTML = modalTemplate.replace('{{title}}', options.title || 'Новое окно');
+        modalHTML = modalHTML.replace('{{content}}', options.content || '');
+        if (options.footerButtons) {
+            for (var i = 0, length = options.footerButtons.length; i < length; i++) {
+                var modalFooterButton = modalButtonTemplate.replace('{{button_class}}', options.footerButtons[i].class);
+                modalFooterButton = modalFooterButton.replace('{{button_handler}}', options.footerButtons[i].handler);
+                modalFooterButton = modalFooterButton.replace('{{button_text}}', options.footerButtons[i].text);
+                modalFooterHTML += modalFooterButton;
+            }
+            modalFooterHTML = modalFooterTemplate.replace('{{buttons}}', modalFooterHTML);
+        }
+        modalHTML = modalHTML.replace('{{footer}}', modalFooterHTML);
+        elemModal.innerHTML = modalHTML;
+        document.body.appendChild(elemModal);
+        return elemModal;
+    }
+
+    function _showModal() {
+        if (!_destroyed && !_hiding) {
+            _elemModal.classList.add('modal__show');
+            document.dispatchEvent(_eventShowModal);
+        }
+    }
+
+    function _hideModal() {
+        _hiding = true;
+        _elemModal.classList.remove('modal__show');
+        _elemModal.classList.add('modal__hiding');
+        setTimeout(function () {
+            _elemModal.classList.remove('modal__hiding');
+            _hiding = false;
+        }, _animationSpeed);
+        document.dispatchEvent(_eventHideModal);
+    }
+
+    function _handlerCloseModal(e) {
+        if (e.target.dataset.dismiss === 'modal') {
+            _hideModal();
+        }
+    }
+
+    _elemModal = _createModal(options || {});
+
+
+    _elemModal.addEventListener('click', _handlerCloseModal);
+    _eventShowModal = new CustomEvent('show.modal', { detail: _elemModal });
+    _eventHideModal = new CustomEvent('hide.modal', { detail: _elemModal });
+
+    return {
+        show: _showModal,
+        hide: _hideModal,
+        destroy: function () {
+            _elemModal.parentElement.removeChild(_elemModal),
+                _elemModal.removeEventListener('click', _handlerCloseModal),
+                _destroyed = true;
+        },
+        setContent: function (html) {
+            _elemModal.querySelector('[data-modal="content"]').innerHTML = html;
+        },
+        setTitle: function (text) {
+            _elemModal.querySelector('[data-modal="title"]').innerHTML = text;
+        }
+    }
+};
+
+let modal = $modal({
+    title: 'Подключите SMARTPOINT.PRO',
+    content: '<p class="modal-body__title">Оставьте контакты, наш специалист расскажет, как легко внедрить сервис на ваш сайт. </p> <fieldset><input placeholder="Введите ваше имя" id="name*" type="text" class="modal-body__name"></input></fieldset><fieldset><input placeholder="Введите ваш телефон*" id="tel" type="tel"class="modal-body__tel"></input></fieldset><fieldset><input placeholder="Адрес сайта*" id="site" type="text" class="modal-body__site"></input></fieldset><div class="modal__agree"><input type="checkbox" name="agree" id="agree" checked="checked"></input><label class="modal__agree-text">Оставляя свои персональные данные, Вы даёте добровольное согласие на их обработку*</label></div>',
+    footerButtons: [
+        { class: 'btn-send', text: 'Оставить бесплатную заявку', handler: 'modalHandlerOk' }
+    ]
+});
+
+let modalButton = document.querySelector('.main-content__button-yellow');
+
+modalButton.addEventListener('click', () => {
+    modal.show();
+})
---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Меняю 
+let parent = document.querySelector('.main-content');
+let buttons = document.querySelector('.main-content__buttons');
+let image = document.querySelector('.main-image');
+
+if (window.innerWidth < 770) {
+    parent.insertBefore(image, buttons);
+}
+
+
+if (window.innerWidth < 420 && window.innerHeight > 700) {
+    document.querySelector('.main-image').style.display = 'block';
+    document.querySelector('.main-image').style.marginLeft = '3rem';
+}
+
+
